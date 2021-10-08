c4438a9e8c5880d41c29a288caf8a4b90fb6eb79
Fernando <fer0osennin@gmail.com>
2021-10-08 14:47:20 -0500

    cambio antes del forwardRef

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,14 +1,14 @@
 import Image from "next/dist/client/image";
 import {ThumbUpIcon} from "@heroicons/react/outline"
-//import { forwardRef } from "react";
+import { forwardRef } from "react";
 
-function Thumbnail  ({ result }) {
+const Thumbnail = forwardRef(({ result },ref) => {
   //console.log(result);
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
   return (
     <div 
-    
+    ref={ref}
      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
@@ -32,6 +32,6 @@ function Thumbnail  ({ result }) {
       </div>
     </div>
   );
-}
+})
 
 export default Thumbnail;
